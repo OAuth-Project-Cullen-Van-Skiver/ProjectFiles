f751a44276ed93f36407650fd74d80fd15891807
CullenVanSkiver <114177084+CullenVanSkiver@users.noreply.github.com>
2023-12-26 22:18:00 -0500

    Add files via upload

--- Google_OAuth_ScholarlyProject.py
+++ Google_OAuth_ScholarlyProject.py
@@ -7,24 +7,29 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #env variable needed for local u
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1' #google has a tendency to return different OAuth scopes that causes an error; this stops that from happening
 
 app = Flask(__name__)
-app.secret_key = "suuupersecret"
+
+app.secret_key = "secretkey" #secret key used for session cookie signing and other symmetric key functionalities
+
 google_blueprint = make_google_blueprint( #blueprint for google oauth; most providers require client_id and client_secret while providing other optional paramters
     client_id="CLIENT_ID", #generated via google cloud console, typically placed in a config file as GOOGLE_OAUTH_CLIENT_ID
     client_secret="CLIENT_SECRET", #generated via google cloud console, typically placed in a config file as GOOGLE_OAUTH_CLIENT_SECRET
     scope=["profile", "email"] #limits applications access to a users account; in this case we can access their profile and email
 )
+
 app.register_blueprint(google_blueprint, url_prefix="/login")
 
 @app.route("/")
+
 def index():
+    
     if not google.authorized:
-        return redirect(url_for("google.login"))
-    response = google_blueprint.session.get("/oauth2/v2/userinfo")
+        return redirect(url_for("google.login")) #redirects user to login page if not authed
+    response = google_blueprint.session.get("/oauth2/v2/userinfo") #gathers user info via Googles oauth2 userinfo API
+    
     if response.ok:
-        userInfo = response.json()
-        #idToken= google.token["id_token"] --> Needs to be decoded
+        userInfo = response.json() ##user info will be in .json format and will need to be decoded to obtain user's ID, email, name, profile picture URL
         return userInfo
-    return "???"
+    return 0
 
 if __name__ == "__main__":
     app.run()
